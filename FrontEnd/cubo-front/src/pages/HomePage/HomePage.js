acba472eabd27fe78b6df4289af38e8b69ff18f1
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2022-10-28 13:09:35 -0300

    H1 e P

--- FrontEnd/cubo-front/src/pages/HomePage/HomePage.js
+++ FrontEnd/cubo-front/src/pages/HomePage/HomePage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import HomeComponents from '../../components/HomeComponents/HomeComponents'
+import { DivH1P, H1, MainContainer, P } from './styled'
 
 const HomePage = () => {
   return (
-    <div>
+    <MainContainer>
+      <DivH1P>
+      <H1>DATA LOAD</H1>
+      <P>Veja a Participação dos Membros na nossa competição de Bebida</P>
+      </DivH1P>
         <HomeComponents />
-    </div>
+    </MainContainer>
   )
 }
 
---
+++ FrontEnd/cubo-front/src/pages/HomePage/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.main`
+display:flex;
+flex-direction: column;
+width: 100%;
+justify-content: center;`
+
+export const H1 = styled.h1`
+font-size: 50px;
+font-family: Arial, Helvetica, sans-serif;
+align-items: center;
+justify-content: center;
+margin-bottom: 1rem;
+text-align: center;`
+
+export const P = styled.p`
+margin-bottom: 3rem;
+align-items: center;
+justify-content: center;
+font-size: 1.5rem;
+text-align: center;`
+
+export const DivH1P = styled.div`
+align-items: center;`
